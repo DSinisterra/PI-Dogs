1a36866000619a990bab22a067156f1ece07fab1
Denis Sinisterra <102484696+DSinisterra@users.noreply.github.com>
2023-03-15 20:37:59 -0500

    Update routes.js

--- PI-Dogs/api/src/routes/routes.js
+++ PI-Dogs/api/src/routes/routes.js
@@ -1,11 +1,33 @@
 const {Router} = require('express');
 const {dogsBddConTemp, getById} = requiere("../controllers/controllers");
 
+//////////////// Ruta para traer todos los perros o por nombre/////////////////////////////////////////////
+
 router.get('/', (req, res) => {
     const { name } = req.query;
-
+    
     try {
+        if (name) {
+            const byName = await getByName(name);
+            res.status(200).send(byName)
+        } else {
+            const allDogs = await getAllDogs();
+            res.status(200).send(allDogs)
+        }
     } catch (error) {
-        return res.status(400).send(error)
+        res.status(400).json({error: error.message})
     }
 });
+
+
+////////////////// Ruta para traer perros por ID ////////////////////////////////////////
+
+
+router.get('/:id', (req, res) => {
+    try {
+        const id = req.params;
+        
+    } catch (error) {
+        
+    }
+})
