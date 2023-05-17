2b5d8620b439766cbd86f52c9889cb311c8014f3
DSinisterra <denissinisterra@gmail.com>
2023-05-17 17:00:50 -0500

    finished with minor bugs

--- PI-Dogs/client/src/components/Card/Card.jsx
+++ PI-Dogs/client/src/components/Card/Card.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Card = ({ id, image, name, temperament }) => {
 
     return (
-        <Link to={`/home/${id}`}>
-            <div className={style.card}>
+        <div className={style.card}>
+            <Link to={`/home/${id}`}>
                 <img className={style.img} src={image} alt={name} />
                 <h3 className={style.name}>{name}</h3>
                 <p className={style.temperament}>Temperament: {temperament}</p>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
--- PI-Dogs/client/src/components/CardsContainer/CardsContainer.jsx
+++ PI-Dogs/client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDogs } from "../../redux/actions";
 import Card from "../Card/Card";
